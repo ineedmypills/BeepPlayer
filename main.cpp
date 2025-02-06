67f14f68ab859cafc15450042aaa54a086bbfd0d
ineedmypills <ineedmypills@outlook.com>
2025-02-06 19:52:51 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,587 @@
+#include <iostream>
+#include <windows.h>
+
+void MarioSong() {
+    system("cls");
+    std::cout << "Enjoy!\n";
+
+    Beep(330, 100);
+    Sleep(100);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(392, 100);
+    Sleep(700);
+    Beep(196, 100);
+    Sleep(700);
+    Beep(262, 300);
+    Sleep(300);
+    Beep(196, 300);
+    Sleep(300);
+    Beep(164, 300);
+    Sleep(300);
+    Beep(220, 300);
+    Sleep(100);
+    Beep(246, 100);
+    Sleep(300);
+    Beep(233, 200);
+    Beep(220, 100);
+    Sleep(300);
+    Beep(196, 100);
+    Sleep(150);
+    Beep(330, 100);
+    Sleep(150);
+    Beep(392, 100);
+    Sleep(150);
+    Beep(440, 100);
+    Sleep(300);
+    Beep(349, 100);
+    Sleep(100);
+    Beep(392, 100);
+    Sleep(300);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(294, 100);
+    Sleep(100);
+    Beep(247, 100);
+    Sleep(500);
+    Beep(262, 300);
+    Sleep(300);
+    Beep(196, 300);
+    Sleep(300);
+    Beep(164, 300);
+    Sleep(300);
+    Beep(220, 300);
+    Sleep(100);
+    Beep(246, 100);
+    Sleep(300);
+    Beep(233, 200);
+    Beep(220, 100);
+    Sleep(300);
+    Beep(196, 100);
+    Sleep(150);
+    Beep(330, 100);
+    Sleep(150);
+    Beep(392, 100);
+    Sleep(150);
+    Beep(440, 100);
+    Sleep(300);
+    Beep(349, 100);
+    Sleep(100);
+    Beep(392, 100);
+    Sleep(300);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(294, 100);
+    Sleep(100);
+    Beep(247, 100);
+    Sleep(900);
+    Beep(392, 100);
+    Sleep(100);
+    Beep(370, 100);
+    Sleep(100);
+    Beep(349, 100);
+    Sleep(100);
+    Beep(311, 100);
+    Sleep(300);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(207, 100);
+    Sleep(100);
+    Beep(220, 100);
+    Sleep(100);
+    Beep(262, 100);
+    Sleep(300);
+    Beep(220, 100);
+    Sleep(100);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(294, 100);
+    Sleep(500);
+    Beep(392, 100);
+    Sleep(100);
+    Beep(370, 100);
+    Sleep(100);
+    Beep(349, 100);
+    Sleep(100);
+    Beep(311, 100);
+    Sleep(300);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(523, 100);
+    Sleep(300);
+    Beep(523, 100);
+    Sleep(100);
+    Beep(523, 100);
+    Sleep(1100);
+    Beep(392, 100);
+    Sleep(100);
+    Beep(370, 100);
+    Sleep(100);
+    Beep(349, 100);
+    Sleep(100);
+    Beep(311, 100);
+    Sleep(300);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(207, 100);
+    Sleep(100);
+    Beep(220, 100);
+    Sleep(100);
+    Beep(262, 100);
+    Sleep(300);
+    Beep(220, 100);
+    Sleep(100);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(294, 100);
+    Sleep(500);
+    Beep(311, 300);
+    Sleep(300);
+    Beep(296, 300);
+    Sleep(300);
+    Beep(262, 300);
+    Sleep(1300);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(262, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(294, 100);
+    Sleep(300);
+    Beep(330, 200);
+    Sleep(50);
+    Beep(262, 200);
+    Sleep(50);
+    Beep(220, 200);
+    Sleep(50);
+    Beep(196, 100);
+    Sleep(700);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(262, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(294, 100);
+    Sleep(100);
+    Beep(330, 100);
+    Sleep(700);
+    Beep(440, 100);
+    Sleep(300);
+    Beep(392, 100);
+    Sleep(500);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(262, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(294, 100);
+    Sleep(300);
+    Beep(330, 200);
+    Sleep(50);
+    Beep(262, 200);
+    Sleep(50);
+    Beep(220, 200);
+    Sleep(50);
+    Beep(196, 100);
+    Sleep(700);
+    /*Intro*/
+    Beep(330, 100);
+    Sleep(100);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(262, 100);
+    Sleep(100);
+    Beep(330, 100);
+    Sleep(300);
+    Beep(392, 100);
+    Sleep(700);
+    Beep(196, 100);
+    Sleep(700);
+    Beep(196, 100);
+    Sleep(125);
+    Beep(262, 100);
+    Sleep(125);
+    Beep(330, 100);
+    Sleep(125);
+    Beep(392, 100);
+    Sleep(125);
+    Beep(523, 100);
+    Sleep(125);
+    Beep(660, 100);
+    Sleep(125);
+    Beep(784, 100);
+    Sleep(575);
+    Beep(660, 100);
+    Sleep(575);
+    Beep(207, 100);
+    Sleep(125);
+    Beep(262, 100);
+    Sleep(125);
+    Beep(311, 100);
+    Sleep(125);
+    Beep(415, 100);
+    Sleep(125);
+    Beep(523, 100);
+    Sleep(125);
+    Beep(622, 100);
+    Sleep(125);
+    Beep(830, 100);
+    Sleep(575);
+    Beep(622, 100);
+    Sleep(575);
+    Beep(233, 100);
+    Sleep(125);
+    Beep(294, 100);
+    Sleep(125);
+    Beep(349, 100);
+    Sleep(125);
+    Beep(466, 100);
+    Sleep(125);
+    Beep(587, 100);
+    Sleep(125);
+    Beep(698, 100);
+    Sleep(125);
+    Beep(932, 100);
+    Sleep(575);
+    Beep(932, 100);
+    Sleep(125);
+    Beep(932, 100);
+    Sleep(125);
+    Beep(932, 100);
+    Sleep(125);
+    Beep(1046, 675);
+}
+
+void JingleBells() {
+    system("cls");
+    std::cout << "Enjoy!\n";
+
+    float $C4 = 261.6;
+    float $C5 = 523.3;
+    float $D4 = 293.7;
+    float $D5 = 587.3;
+    float $E4 = 329.6;
+    float $E5 = 659.3;
+    float $F5 = 698.5;
+    float $G5 = 784.0;
+
+    Beep($E5, 500);
+    Beep($E5, 500);
+    Beep($E5, 500);
+
+    Sleep(500);
+
+    Beep($E5, 500);
+    Beep($E5, 500);
+    Beep($E5, 500);
+
+    Sleep(500);
+
+    Beep($E5, 500);
+    Beep($G5, 500);
+    Beep($C5, 500);
+    Beep($D5, 500);
+    Beep($E5, 500);
+
+    Beep($C4, 500);
+    Beep($D4, 500);
+    Beep($E4, 500);
+
+    Beep($F5, 500);
+    Beep($F5, 500);
+    Beep($F5, 500);
+
+    Sleep(500);
+
+    Beep($F5, 500);
+    Beep($E5, 500);
+    Beep($E5, 500);
+
+    Sleep(500);
+
+    Beep($E5, 500);
+    Beep($D5, 500);
+    Beep($D5, 500);
+    Beep($E5, 500);
+    Beep($D5, 500);
+
+    Sleep(500);
+
+    Beep($G5, 500);
+}
+
+void LaguDoraemon() {
+    system("cls");
+    std::cout << "Enjoy!\n";
+
+    Beep(220, 300);
+
+    Beep(294, 300);
+
+    Beep(294, 300);
+
+    Beep(370, 300);
+
+    Beep(494, 300);
+
+    Beep(370, 300);
+
+    Beep(440, 800);
+
+    /* */
+
+    Beep(440, 300);
+
+    Beep(494, 300);
+
+    Beep(440, 300);
+
+    Beep(370, 300);
+
+    Beep(392, 300);
+
+    Beep(370, 300);
+
+    Beep(330, 800);
+
+    /* */
+
+    Beep(247, 300);
+
+    Beep(330, 300);
+
+    Beep(330, 300);
+
+    Beep(370, 300);
+
+    Beep(555, 300);
+
+    Beep(555, 300);
+
+    Beep(494, 300);
+
+    Beep(440, 300);
+
+    Beep(392, 800);
+
+    Beep(392, 300);
+
+    Beep(370, 300);
+
+    Beep(247, 800);
+
+    Beep(278, 300);
+
+    Beep(294, 300);
+
+    Beep(330, 2600);
+
+    /* */
+
+    Beep(220, 300);
+
+    Beep(294, 300);
+
+    Beep(294, 300);
+
+    Beep(370, 300);
+
+    Beep(494, 300);
+
+    Beep(370, 300);
+
+    Beep(440, 800);
+
+    /* */
+
+    Beep(440, 300);
+
+    Beep(494, 300);
+
+    Beep(440, 300);
+
+    Beep(370, 300);
+
+    Beep(392, 300);
+
+    Beep(370, 300);
+
+    Beep(330, 800);
+
+    /* */
+
+    Beep(247, 300);
+
+    Beep(330, 300);
+
+    Beep(330, 300);
+
+    Beep(370, 300);
+
+    Beep(555, 300);
+
+    Beep(555, 300);
+
+    Beep(494, 300);
+
+    Beep(440, 300);
+
+    Beep(392, 800);
+
+    Beep(392, 300);
+
+    Beep(370, 300);
+
+    Beep(278, 600);
+
+    Beep(330, 600);
+
+    Beep(294, 2600);
+
+    /*Reff : */
+
+    Beep(494, 300);
+
+    Beep(494, 300);
+
+    Beep(494, 300);
+
+    Beep(440, 300);
+
+    Beep(392, 200);
+
+    Beep(440, 200);
+
+    Beep(494, 200);
+
+    Beep(440, 800);
+
+    Beep(330, 300);
+
+    Beep(370, 300);
+
+    Beep(416, 300);
+
+    Beep(330, 300);
+
+    Beep(440, 2000);
+
+    /* */
+
+    Beep(494, 800);
+
+    Beep(440, 800);
+
+    Beep(392, 1600);
+
+    /* */
+
+    Beep(555, 300);
+
+    Beep(555, 300);
+
+    Beep(555, 300);
+
+    Beep(494, 300);
+
+    Beep(440, 300);
+
+    Beep(494, 300);
+
+    Beep(440, 300);
+
+    Beep(392, 1400);
+
+    /* */
+
+    Beep(440, 300);
+
+    Beep(494, 300);
+
+    Beep(370, 1100);
+
+    Beep(330, 300);
+
+    Beep(294, 1800);
+
+    /* */
+
+    Beep(494, 800);
+
+    Beep(440, 800);
+
+    Beep(392, 1600);
+
+    /* */
+
+    Beep(555, 300);
+
+    Beep(555, 300);
+
+    Beep(555, 300);
+
+    Beep(494, 300);
+
+    Beep(440, 300);
+
+    Beep(494, 300);
+
+    Beep(440, 300);
+
+    Beep(392, 1400);
+
+    /* */
+
+    Beep(440, 300);
+
+    Beep(494, 300);
+
+    Beep(370, 1100);
+
+    Beep(330, 300);
+
+    Beep(294, 1800);
+}
+
+int main() {
+    int choose{};
+
+    while (true) {
+        system("cls");
+        std::cout << "Welcome to Beep Player!\n";
+        std::cout << "1. Mario Song\n2. JingleBells\n3. Lagu Doraemon\n4. Exit\n";
+        std::cin >> choose;
+        if (choose == 1) {
+            MarioSong();
+        } else if (choose == 2) {
+            JingleBells();
+        } else if (choose == 3) {
+            LaguDoraemon();
+        } else if (choose == 4) {
+            return 0;
+        } else { std::cout << "Invalid Choice" << std::endl; }
+    }
+
+    return 0;
+}
